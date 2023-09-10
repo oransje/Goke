331710fb953696a8e2a71c4ba52bdec91f6cf742
vsantos1 <vinicius.shyn2@gmail.com>
2023-09-10 19:37:37 -0300

    feat: app const variables

---
+++ cmd/constants.go
@@ -0,0 +1,14 @@
+package cmd
+
+const TODO_DUMMY = `CREATE TABLE IF NOT EXISTS todo(
+    id int NOT NULL,
+    title varchar(255),
+    content varchar(255),
+    created_at datetime,
+    updated_at datetime,
+    done boolean DEFAULT false,
+    PRIMARY KEY(ID)
+)
+`
+
+const VERSION = "v0.1.2"
